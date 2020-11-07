257459dab41716b5454356036b62a7e7e54fbd6a
Vincent <vincent.breysse99@gmail.com>
2020-11-07 16:43:18 +0100

    Setup Sphere occluder stuff

--- Assets/Scripts/CullingSystem.cs
+++ Assets/Scripts/CullingSystem.cs
@@ -16,32 +16,44 @@ public class CullingSystem : SystemBase
         var entityOutFrumstrumColor = Main.EntityOutFrumstrumColor;
         var entityInFrumstrumColor = Main.EntityInFrustrumColor;
 
+        var occluderQuery = GetEntityQuery(typeof(WorldOccluderRadius), typeof(Translation));
+
         var frustrumPlanes = new NativeArray<Plane>(Main.FrustrumPlanes, Allocator.TempJob);
+        //var occluderTranslations = occluderQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+        //var occluderRadiuses = occluderQuery.ToComponentDataArray<WorldOccluderRadius>(Allocator.TempJob);
 
         this.Entities
         .WithAll<EntityTag>()
         .WithReadOnly(frustrumPlanes)
+        /*.WithReadOnly(occluderTranslations)
+        .WithReadOnly(occluderRadiuses)*/
         .ForEach((ref URPMaterialPropertyBaseColor color, in Translation translation, in WorldBoundingRadius radius) =>
         {
-            bool inFrustrum = true;
-
-            for (int i = 0; i < 6; ++i)
-            {
-                float3 normal = frustrumPlanes[i].normal;
-                float distance = frustrumPlanes[i].distance;
-                float3 point = -normal * distance;
-                var delta = translation.Value - point;
-
-                if (math.dot(normal, delta) < -radius.Value)
-                {
-                    inFrustrum = false;
-                    break;
-                }
-            }
+            bool inFrustrum = IsInFrustrum(translation.Value, radius.Value, frustrumPlanes);
 
             color.Value = inFrustrum ? entityInFrumstrumColor : entityOutFrumstrumColor;
         })
+        /*.WithDisposeOnCompletion(occluderRadiuses)
+        .WithDisposeOnCompletion(occluderTranslations)*/
         .WithDisposeOnCompletion(frustrumPlanes)
         .ScheduleParallel();
+    }
+
+    static bool IsInFrustrum(float3 center, float radius, in NativeArray<Plane> planes)
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            float3 normal = planes[i].normal;
+            float distance = planes[i].distance;
+            float3 point = -normal * distance;
+            var delta = center - point;
+
+            if (math.dot(normal, delta) < -radius)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
---
+++ Assets/Scripts/WorldOccluderRadiusAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class WorldOccluderRadiusAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var sphereCollider = GetComponentInChildren<SphereCollider>();
+        if (sphereCollider == null) return;
+
+        var scale = this.transform.lossyScale;
+        var maxScale = math.max(math.max(scale.x, scale.y), scale.z);
+
+        var radius = new WorldOccluderRadius
+        {
+            Value = sphereCollider.radius * maxScale
+        };
+
+        dstManager.AddComponentData(entity, radius);
+    }
+}
