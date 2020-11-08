a77666604089d05c0173ee4ee2adc588d092932f
Vincent <vincent.breysse99@gmail.com>
2020-11-08 20:19:12 +0100

    Create WorldOccluderExtents component and setup scene for testing

---
+++ Assets/Scripts/WorldOccluderExtents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct WorldOccluderExtents : IComponentData
+{
+    public float3 Right;
+    public float3 Up;
+}
---
+++ Assets/Scripts/WorldOccluderExtentsAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class WorldOccluderExtentsAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var scale = this.transform.lossyScale;
+
+        var extents = new WorldOccluderExtents
+        {
+            Right = this.transform.right * scale.x * 5f,
+            Up = this.transform.up * scale.y * 5f
+        };
+
+        dstManager.AddComponentData(entity, extents);
+    }
+}
