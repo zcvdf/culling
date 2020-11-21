8502145cf2a1b2ae04152eec22a0eaa85d52bf95
Vincent <vincent.breysse99@gmail.com>
2020-11-21 15:46:00 +0100

    Add EntityCullingResult component to Entity prefab

---
+++ Assets/Scripts/EntityCullingResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public enum CullingResult
+{
+    NotCulled,
+    CulledByOctree,
+    CulledByFrustrumPlanes,
+    CulledBySphereOccluder,
+    CulledByQuadOccluder,
+}
+
+[GenerateAuthoringComponent]
+public class EntityCullingResult : IComponentData
+{
+    public CullingResult Value;
+}
