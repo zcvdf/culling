7084f1e974b8f04eec57adf349f8c94f80dc230f
Vincent <vincent.breysse99@gmail.com>
2020-11-22 00:41:56 +0100

    More Burst friendly color update

--- Assets/Scripts/UpdateEntityColor.cs
+++ Assets/Scripts/UpdateEntityColor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Rendering;
 using UnityEngine;
 
@@ -13,20 +15,23 @@ public class UpdateEntityColor : SystemBase
         var inFrumstrumColor = Main.EntityInFrustrumColor;
         var occludedColor = Main.EntityOccludedColor;
 
+        var colors = new NativeArray<float4>(6, Allocator.TempJob);
+        colors[0] = inFrumstrumColor;
+        colors[1] = outFrumstrumColor;
+        colors[2] = outFrumstrumColor;
+        colors[3] = outFrumstrumColor;
+        colors[4] = occludedColor;
+        colors[5] = occludedColor;
+
         this.Entities
         .WithAll<EntityTag>()
+        .WithReadOnly(colors)
         .ForEach((ref URPMaterialPropertyBaseColor color, in EntityCullingResult cullingResult) =>
         {
-            switch (cullingResult.Value)
-            {
-                case CullingResult.CulledByOctreeClusters:  color.Value = outFrumstrumColor;    break;
-                case CullingResult.CulledByOctreeNodes:     color.Value = outFrumstrumColor;    break;
-                case CullingResult.CulledByFrustrumPlanes:  color.Value = outFrumstrumColor;    break;
-                case CullingResult.CulledByQuadOccluder:    color.Value = occludedColor;        break;
-                case CullingResult.CulledBySphereOccluder:  color.Value = occludedColor;        break;
-                case CullingResult.NotCulled:               color.Value = inFrumstrumColor;     break;
-            }
+            var id = (int)cullingResult.Value;
+            color.Value = colors[id];
         })
+        .WithDisposeOnCompletion(colors)
         .ScheduleParallel();
     }
 }
