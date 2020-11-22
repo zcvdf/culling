a1b7e3b85581e96b42605bd3ce4aec8920bf8042
Vincent <vincent.breysse99@gmail.com>
2020-11-22 22:47:31 +0100

    Remove collider from Sphere occluder prefab

--- Assets/Scripts/WorldOccluderRadiusAuthoring.cs
+++ Assets/Scripts/WorldOccluderRadiusAuthoring.cs
@@ -8,15 +8,12 @@ public class WorldOccluderRadiusAuthoring : MonoBehaviour, IConvertGameObjectToE
 {
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        var sphereCollider = GetComponentInChildren<SphereCollider>();
-        if (sphereCollider == null) return;
-
         var scale = this.transform.lossyScale;
         var maxScale = math.max(math.max(scale.x, scale.y), scale.z);
 
         var radius = new WorldOccluderRadius
         {
-            Value = sphereCollider.radius * maxScale
+            Value = 0.5f * maxScale
         };
 
         dstManager.AddComponentData(entity, radius);
